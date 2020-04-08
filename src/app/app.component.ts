852c7019bfddf75674fd18631634852c9e9d6ffe
Kevin Zhou <kevzho141@gmail.com>
2020-04-07 19:08:29 -0700

    angular redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'stipend-calculator';
+  startDate: Date = null
+  endDate: Date = null
+  stipend: number = null
+  calculate() {
+    if (this.startDate > this.endDate || this.stipend <= 0) {
+      alert('wrong bounds')
+      return
+    }
+    let out: number = (new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 0).getDate() - this.startDate.getDate()) / new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 0).getDate()
+    if (this.startDate.getFullYear() === this.endDate.getFullYear()) {
+      out += this.endDate.getMonth() - this.startDate.getMonth() - 1
+    } else {
+      out += 11 - this.startDate.getMonth()
+      out += this.endDate.getMonth()
+      out += (this.endDate.getFullYear() - this.startDate.getFullYear() - 1) * 12
+    }
+    out += (this.endDate.getDate() + 1) / new Date(this.endDate.getFullYear(), this.endDate.getMonth() + 1, 0).getDate()
+    document.querySelector('#result').innerHTML = `\~\$${out * (this.stipend)}`
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
